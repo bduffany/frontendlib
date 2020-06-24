0a00d3a8fe7be2c2b779898b510830bce6fb7f80
Brandon Duffany <brandonduffany@gmail.com>
2020-06-24 19:10:41 -0400

    Fix version bump logic

--- bin/release.js
+++ bin/release.js
@@ -3,13 +3,13 @@ const { readFileSync } = require('fs');
 const { program } = require('commander');
 
 program.option('bump', 'Version to bump [major/minor/patch]');
-program.parse();
 
 async function run(command, args) {
   console.log(`RUNNING: ${command}`);
   return new Promise((resolve, reject) => {
     exec(command, (error, stdout, stderr) => {
       if (error) {
+        console.error(stderr);
         reject(error);
       }
       resolve(stdout);
@@ -18,6 +18,13 @@ async function run(command, args) {
 }
 
 async function main() {
+  program.parse();
+
+  if (!program.bump) {
+    console.error('ERROR: version type not specified.');
+    process.exit(1);
+  }
+
   try {
     await run('tsc');
   } catch (e) {
@@ -42,20 +49,25 @@ async function main() {
     .split('.')
     .map((value) => Number(value));
 
-  const newVersion =
-    program.bump === 'major'
-      ? [++major, 0, 0]
-      : program.bump === 'minor'
-      ? [major, ++minor, 0]
-      : [major, minor, ++patch];
+  const newVersion = (program.bump === 'major'
+    ? [++major, 0, 0]
+    : program.bump === 'minor'
+    ? [major, ++minor, 0]
+    : [major, minor, ++patch]
+  ).join('.');
+
   const newBranch = `dev-${newVersion}`;
-  await run(`git checkout -b ${newBranch}`);
   await run(
     `perl -p -i -e 's@"version": "${currentVersion}"@"version": "${newVersion}"@' package.json`
   );
-  await run(
-    `git add . && git commit -m "Publish dev version ${newVersion}" && git push --set-upstream origin ${newBranch}`
-  );
+  await run(`git add . && git commit -m "Publish dev version ${newVersion}"`);
+  try {
+    await run(
+      `git checkout -b ${newBranch} && git push --set-upstream origin ${newBranch}`
+    );
+  } finally {
+    await run('git checkout master');
+  }
 }
 
 main();
