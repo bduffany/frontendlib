b12db07ae123fa83790d53577a20e885d4755614
Brandon Duffany <brandon@buildbuddy.io>
2022-03-20 12:57:27 -0400

    Improve flexibility

--- src/react/hooks/useLocalStorage.ts
+++ src/react/hooks/useLocalStorage.ts
@@ -1,17 +1,34 @@
-import { Dispatch, useState, useEffect, useCallback } from 'react';
+import {
+  Dispatch,
+  SetStateAction,
+  useState,
+  useEffect,
+  useCallback,
+} from 'react';
 
 export default function useLocalStorage(
   key: string,
   initialValue: string = ''
-): [string, Dispatch<string>] {
+): [string, Dispatch<SetStateAction<string>>] {
   const [value, setValue] = useState(
     () => window.localStorage.getItem(key) || initialValue
   );
 
-  const setItem = (newValue: string) => {
-    setValue(newValue);
-    window.localStorage.setItem(key, newValue);
-  };
+  const setItem: Dispatch<SetStateAction<string>> = useCallback(
+    (valueOrFn) => {
+      setValue((current: string) => {
+        let newValue: string;
+        if (typeof valueOrFn === 'string') {
+          newValue = valueOrFn;
+        } else {
+          newValue = valueOrFn(current);
+        }
+        window.localStorage.setItem(key, newValue);
+        return newValue;
+      });
+    },
+    [setValue]
+  );
 
   useEffect(() => {
     const newValue = window.localStorage.getItem(key);
